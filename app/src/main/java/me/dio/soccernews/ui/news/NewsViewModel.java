0b884f659fe2a3e65af469cf9996e20c6d107585
Anderson <andersons1lva@yahoo.com.br>
2022-07-27 22:43:45 -0300

    Inclusão do Retrofit e Picasso.

--- app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -7,22 +7,45 @@ import androidx.lifecycle.ViewModel;
 import java.util.ArrayList;
 import java.util.List;
 
+import me.dio.soccernews.data.remote.SoccerNewsApi;
 import me.dio.soccernews.doman.News;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<List<News>> news;
+    private final MutableLiveData<List<News>> news = new MutableLiveData<>();
+    private final SoccerNewsApi api;
 
     public NewsViewModel() {
-        this.news = new MutableLiveData<>();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://andersons1lva.github.io/soccer-news-api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        //TODO Remover MOck de Notícias
-        List<News> news = new ArrayList<>();
-        news.add(new News("Onda de lesões desfalca seleções nas competições internacionais do futebol feminino", "Sem Copa do Mundo nem qualquer outra competição de relevo no futebol masculino, julho de 2022 virou o mês das mulheres. Quatro campeonatos continentais femininos de seleções serão disputados simultaneamente: Copa América, Eurocopa, Copa Africana de Nações e o Campeonato da Concacaf. Mas o desfile de craques mundiais já está seriamente prejudicado por causa de uma onda de lesões."));
-        news.add(new News("Resistência feminina", "Férias coletivas, salários atrasados e o investimento de R$ 40 mil por mês foram as justificativas do Sport - em 2019 - para dissolver o futebol feminino. O risco de punição - na época - interrompeu o processo, mas o departamento nunca mais foi o mesmo. Desde então, resiste. Três anos após a decepção, elas enfim voltam a comemorar."));
-        news.add(new News("Me chamaram de macaca", "Natasha do Nascimento relatou gritos racistas da torcida na partida contra o Sport, na Ilha do Retiro, pelas oitavas de final do Brasileiro Feminino da Série A-3. A atacante atua pelo Doce Mel, da Bahia, e revelou o caso em entrevista ao ge, no último domingo. Ela também relatou o caso para a árbitra Deborah Cecilia, que conduziu o duelo e registrou o ocorrido na súmula."));
+        api = retrofit.create(SoccerNewsApi.class);
+        this.findNews();
+    }
 
-        this.news.setValue(news);
+    private void findNews() {
+        api.getNews().enqueue(new Callback<List<News>>() {
+            @Override
+            public void onResponse(Call<List<News>> call, Response<List<News>> response) {
+                if (response.isSuccessful()){
+                    news.setValue(response.body());
+                }else{
+                    //TODO Pensar em uma estratégia de tratamento de erros.
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<News>> call, Throwable t) {
+                //TODO Pensar em uma estratégia de tratamento de erros.
+            }
+        });
     }
 
     public LiveData<List<News>> getNews() {
